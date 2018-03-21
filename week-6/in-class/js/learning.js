6837e8bbc56ee1811e13539649b1f3966e17c182
mckissic <mckissic@simmons.edu>
2018-03-20 20:48:26 -0400

    First JavaScript in-class assignment

--- week-6/in-class/js/learning.js
+++ week-6/in-class/js/learning.js
@@ -1,2 +1,31 @@
 // "use strict" tells the browser to enforce some rules about what can be in our JavaScript.
 "use strict";
+
+let myName = "Sophia";
+let yourName = "Chris";
+
+function helloWorld() {
+  renderOutput("Hello World");
+}
+function helloName(name) {
+  renderOutput("Hello " + name);
+}
+function todaysHours(oneDaysHours) {
+  renderOutput(oneDaysHours);
+}
+
+if (myName === "Sophia") {
+  renderOutput("That's me!")
+} else if (myName === "Chris") {
+  renderOutput("That's Chris!")
+} else {
+  renderOutput("That's not me!")
+}
+
+helloName("Sophia");
+helloName("Chris");
+helloName("Renee");
+
+//todaysHours("Tuesday: 9a - 5p");
+
+hours.forEach(todaysHours);
