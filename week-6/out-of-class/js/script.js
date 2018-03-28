6f77a335b57d1d24af0f89b8eb142b261a7c328b
mckissic <mckissic@simmons.edu>
2018-03-27 20:53:46 -0400

    Saved my work for in-class assignment 6

--- week-6/out-of-class/js/script.js
+++ week-6/out-of-class/js/script.js
@@ -4,3 +4,29 @@ function renderOutput(output) {
     let output_element = document.getElementById("output");
     output_element.innerHTML += "<div>" + output + "</div>"
 }
+
+let hours = [
+    "Monday: 9a - 9p",
+    "Tuesday: 9a - 9p",
+    "Wednesday: 9a - 9p",
+    "Thursday: 9a - 9p",
+    "Friday: 9a - 5p",
+    "Saturday: 9a - 5p",
+    "Sunday: closed"
+];
+
+function displayOneDaysHours(one_days_hours) {
+  let parts_of_hours = one_days_hours.split(": ");
+  if (parts_of_hours[1] === "closed") {
+    renderOutput("On " + parts_of_hours[0] + " library is " + parts_of_hours[1]);
+  } else {
+    renderOutput("On " + parts_of_hours[0] + " the hours are " + parts_of_hours[1]);
+  }
+}
+
+//Display the week's hours to the user
+function weeklyHours(hours) {
+  hours.forEach(displayOneDaysHours);
+}
+
+weeklyHours(hours);
--- week-7/js/catalog.js
+++ week-7/js/catalog.js
@@ -50,3 +50,10 @@ let the_philadelphia_story = {
 let books = [native_son, a_tree_grows_in_brooklyn, the_stranger];
 
 let films = [the_third_man, rebecca, the_philadelphia_story];
+
+let result_list = document.querySelector(".result-list");
+
+
+function displayOneBook(book) {
+  result_list.innerHTML = "<li><em>" + book.title + "</em>, " + book.author + ", " + book.year + "</li>";
+}
